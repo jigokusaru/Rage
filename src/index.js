f3121121384fdc7ebe644fc248ae04034290e75c
Abbey Joubert <joubertjr.abbey@gmail.com>
2024-04-25 23:16:21 -0400

    reboot

--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
-require("dotenv").config();
-const { Client, GatewayIntentBits } = require("discord.js");
-const { token } = require("../config.json");
-const cmdHandler = require("./util/cmdHandler.js");
+// index.js
+
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ], // Add your desired intents here
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ] // Add your desired intents here
 });
 
-client.on("ready", () => {
-  console.log(`${client.user.username} is ready!`);
+// Event: When the bot is ready
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("messageCreate", (msg) => {
-  if (msg.author.bot) return;
-  if (msg.content.startsWith(`<@${client.user.id}>`)) {
-    const args = msg.content
-      .slice(`<@${client.user.id}>`.length)
-      .trim()
-      .split(/ +/)
-      .filter((arg) => arg.trim());
-    //const command = args.shift().toLowerCase();
-    if (args.length > 0) {
-      cmdHandler.runCommand(msg, args);
-    } else {
-      msg.reply("What do you want!?");
+// Event: When a message is created
+client.on('messageCreate', (message) => {
+    // Ignore messages from bots
+    if (message.author.bot) return;
+
+    // Check if the message starts with the bot mention (prefix)
+    const botMention = `<@${client.user.id}>`;
+    if (message.content.startsWith(botMention)) {
+      
     }
-  }
+        
 });
 
+// Log in to Discord with the bot token from .env
 client.login(process.env.TOKEN);
